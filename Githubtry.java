98312e0751ac2b4bd12cc44daf8477cb22777956
kamalrathod <kamalrathod23@gmail.com>
2021-01-05 13:30:16 +0530

    some changes

---
+++ Githubtry.java
@@ -0,0 +1,21 @@
+public class Githubtry {
+
+
+public void status(){
+
+System.out.println("Git Hub class in working");
+
+}
+
+public static void main (String[] args){
+
+Githubtry obj1 = new Githubtry();
+
+obj1.status();
+
+
+}
+
+
+
+}
